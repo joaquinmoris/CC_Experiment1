3a000c02ecc1412c90ae39fd1ed1560fcb8b4c80
Joaquín Morís <joaquin.moris+GITHUB@gmail.com>
2020-01-28 15:31:18 +0100

    Scrip to download completed. Tokens are saved offline in the tokens folder

---
+++ scripts/download.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(googledrive)
+
+for (cur_account in 1:10){
+  googledrive::drive_auth(email = paste0('sotano1pc', cur_account, '@gmail.com'))
+  data_files <- drive_find(pattern = 'CCTest2020', type = 'csv')
+
+  if (nrow(data_files)){
+    for (cur_row in 1:nrow(data_files)){
+      drive_download(file =  data_files[cur_row, ],
+                     path = paste0('./input/',  data_files[cur_row, ]$name),
+                     overwrite = TRUE)
+    }
+  }
+}
